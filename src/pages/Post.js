7bf7f13378c065e856e1c244e0a2b5b338a198ec
Terlale <trllramazanova@gmail.com>
2023-04-29 16:06:05 +0400

    post ve category

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,7 +1,22 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Post = () => {
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate("/");
+  };
+
+  const goPost = () => {
+    navigate("/post");
+  };
+
+  const goCategory = () => {
+    navigate("/category");
+  };
+
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
 
@@ -16,26 +31,70 @@ const Post = () => {
       });
     setName("");
     setDescription("");
+  };
   return (
     <div>
-    Name:
-    <input
-      type="text"
-      value={name}
-      onChange={(e) => setName(e.target.value)}
-    />
-    Description : <br />
-    <input
-      type="text"
-      value={description}
-      onChange={(e) => setDescription(e.target.value)}
-    />
-    <button type="submit" onClick={handleSubmit}>
-      Submit
-    </button>
-  </div>
-  )
-}
-}
+      <div
+        style={{
+          backgroundColor: "#E8960F",
+          color: "white",
+          display: "flex",
+          justifyContent: "space-between",
+          textAlign: "center",
+          padding: "20px 60px",
+        }}
+      >
+        <div>
+          <h2 onClick={goHome}>Home</h2>
+        </div>
+        <div style={{ display: "flex", gap: "25px" }}>
+          <h3 onClick={goCategory}>Category</h3>
+          <h3 onClick={goPost} style={{ color: "red" }}>
+            Post
+          </h3>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          "align-items": "center",
+          "justify-content": "center",
+          "flex-direction": "column",
+          gap: "5px",
+          padding: "7% 0%",
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          style={{ width: "270px", height: "40px" }}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          style={{ width: "270px", height: "40px" }}
+        />
+        <br />
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          style={{
+            width: "275px",
+            height: "40px",
+            "background-color": "blue",
+            color: "white",
+          }}
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Post
+export default Post;
